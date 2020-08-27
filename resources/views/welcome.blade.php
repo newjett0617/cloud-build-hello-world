213cad367d0ca7c5a39907c9ee2a12fe3404aa30
Jett <>
2020-08-28 00:27:08 +0800

    fix

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel v1
+                    Laravel v2
                 </div>
 
                 <div class="links">
